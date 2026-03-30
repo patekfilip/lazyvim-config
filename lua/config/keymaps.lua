0adbba487b9f4642026aea3eda5dca04d64b9921
Filip Lakic <filip.lakic@egeli-informatik.ch>
2026-03-30 12:55:13 +0200

    feat: add terminal handling

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -10,3 +10,11 @@ end, { desc = "Go to next diagnostic" })
 vim.keymap.set("n", "gE", function()
   vim.diagnostic.jump({ count = -1 })
 end, { desc = "Go to previous diagnostic" })
+
+vim.keymap.set("n", "<leader>th", "<cmd>split | terminal<cr>", { desc = "Terminal horizontal" })
+vim.keymap.set("n", "<leader>tv", "<cmd>vsplit | terminal<cr>", { desc = "Terminal vertical" })
+vim.keymap.set("n", "<leader>tf", "<cmd>terminal<cr>", { desc = "Terminal (current window)" })
+
+vim.keymap.set("t", "<C-Space>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+vim.keymap.set("t", "<Nul>", [[<C-\><C-n>]], { desc = "Exit terminal mode (Ctrl-Space fallback)" })
+vim.keymap.set("t", "<Esc><Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
